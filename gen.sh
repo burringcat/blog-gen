56b2b05be8cb6f08b370cb35332b120c87aaff38
AW <ntkomataus@gmail.com>
2021-05-27 16:42:17 +0800

    update

--- gen.sh
+++ gen.sh
@@ -40,7 +40,7 @@ do
     post_file_name="$(basename $p .html)"
     bold_title $p > $POST_TEMP_PATH
     POST_RENDERED="$(contentize $POST_TEMP_PATH)"
-    INFO="<a href='index.html' style='font-size:1.3em'>Back</a><br><span>(Date: ${post_file_name})</span>"
+    INFO="<a href='index.html' style='font-size:1.3em'>Back</a><br><span>(Date: ${post_file_name})</span> Generated with <a href='https://github.com/thekonungr/blog-gen'>blog-gen</a>"
     POST_RENDERED="$INFO<br><hr>$POST_RENDERED<br><hr>$INFO"
     HTML=$(perl -pe "s|__BODY__|$POST_RENDERED|;s|__TITLE__|$(post_title $p)|" "$BASE_HTML")
     echo $HTML > $OUTPUT_DIR/$(basename $p)
